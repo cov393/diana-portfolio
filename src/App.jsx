c5730e101658182926d96f8769ce641ff3b4485a
Diana Percatkina <covuk393@gmail.com>
2025-01-23 19:30:07 +0000

    Hint Feature for icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import About from './components/About';
-// import Technologies from './components/Technologies';
-// import Experience from './components/Experience';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-// import MyTimeline from './components/TimeLine';
-// import ReactGA from 'react-ga';
-
-// const TRACKING_ID = "G-Q2WPE13J4P";
-// ReactGA.initialize(TRACKING_ID);
-
-// const App = () => {
-//   return (
-//   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>  
-//     <div className='fixed top-0 -z-10 h-full w-full'>
-//       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-//     </div>
-    
-//     <div className='container mx-auto px-8'>
-//       <Navbar />
-//       <Hero />
-//       <About />
-//       <MyTimeline />
-//       <Experience />
-//       <Technologies />
-//       <Projects />
-//       <Contact />
-//     </div>
-//   </div>
-//   );
-// };
-// export default App
-
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -50,7 +14,6 @@ const TRACKING_ID = "G-Q2WPE13J4P";
 ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
-  // Проверка ширины экрана: <768px — это считается телефоном.
   const isMobile = useMediaQuery('(max-width:768px)');
 
   return (
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,3 +1,6 @@
+import {motion} from "framer-motion";
+
+
 import {RiReactjsLine} from "react-icons/ri"
 import { FaNodeJs } from "react-icons/fa";
 import {BiLogoPostgresql} from "react-icons/bi"
@@ -19,8 +22,6 @@ import { FaAws } from "react-icons/fa";
 import { FaWindows } from "react-icons/fa";
 import { FcLinux } from "react-icons/fc";
 import { SiNetapp } from "react-icons/si";
-
-import {motion} from "framer-motion";
 
 const getRandomDuration = () => Math.floor(Math.random() * 10) + 1;
 
@@ -59,64 +60,63 @@ import { PiFileCSharp } from "react-icons/pi";const Technologies = () => {
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <VscTerminalBash className="text-3xl text-white-400"/>
+                    <span className="tooltip-text">Bash</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <IoLogoJavascript className="text-3xl text-yellow-300"/>
+                    <span className="tooltip-text">JavaScript</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <PiFileCSharp className="text-3xl text-violet-500"/>
+                    <span className="tooltip-text">C#</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <FaPython className="text-3xl text-green-500"/>
+                    <span className="tooltip-text">Python</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
-                    <FaNodeJs className="text-3xl text-green-500"/>
-                </motion.div>
-
-                <motion.div 
-                    variants={iconVariants()}
-                    initial="initial"
-                    animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <BiLogoPostgresql className="text-3xl text-sky-700"/>
+                    <span className="tooltip-text">SQL</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <SiTerraform className="text-3xl text-violet-700"/>
+                    <span className="tooltip-text">Terraform</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                    className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <TbBrandPowershell className="text-3xl text-violet-700"/>
+                    <span className="tooltip-text">Powershell</span>
                 </motion.div>
             </motion.div>
 
@@ -132,64 +132,74 @@ import { PiFileCSharp } from "react-icons/pi";const Technologies = () => {
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                    className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container"
+                     >
                     <FaBootstrap className="text-3xl text-violet-600"/>
+                    <span className="tooltip-text">Bootstrap</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <DiDjango className="text-3xl text-green-400"/>
+                    <span className="tooltip-text">Django</span>
+
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <AiOutlineDotNet className="text-3xl text-sky-500"/>
+                    <span className="tooltip-text">. NET</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <SiFlask  className="text-3xl text-red-700"/>
+                    <span className="tooltip-text">Flask</span>
                 </motion.div>
                 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <IoLogoFirebase className="text-3xl text-yellow-500"/>
+                    <span className="tooltip-text">Firebase</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <RiReactjsLine className="text-3xl text-cyan-400"/>
+                    <span className="tooltip-text">ReactJS</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <FaNodeJs className="text-3xl text-green-500"/>
+                    <span className="tooltip-text">NodeJS</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <TbBrandXamarin className="text-3xl text-sky-700"/>
+                    <span className="tooltip-text">Xamarin</span>
                 </motion.div>
             </motion.div>
 
@@ -205,32 +215,36 @@ import { PiFileCSharp } from "react-icons/pi";const Technologies = () => {
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
-                    <VscAzure className="text-3xl text-sky-500" data-tip="ReactJS" />
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
+                    <VscAzure className="text-3xl text-sky-500" />
+                    <span className="tooltip-text">Azure</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <SiGooglecloud className="text-3xl text-cyan-500"/>
+                    <span className="tooltip-text">Google Cloud</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <SiHeroku className="text-3xl text-violet-400"/>
+                    <span className="tooltip-text">Heroku</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <FaAws className="text-3xl text-white-300"/>
+                    <span className="tooltip-text">AWS</span>
                 </motion.div>
 
             </motion.div>
@@ -248,24 +262,27 @@ import { PiFileCSharp } from "react-icons/pi";const Technologies = () => {
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <FaWindows className="text-3xl text-cyan-400"/>
+                    <span className="tooltip-text">Windows</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <FcLinux className="text-3xl"/>
+                    <span className="tooltip-text">Linux</span>
                 </motion.div>
 
                 <motion.div 
                     variants={iconVariants()}
                     initial="initial"
                     animate="animate"
-                     className="rounded-2xl border-4 border-neural-800 p-4">
+                     className="rounded-2xl border-4 border-neural-800 p-4 relative tooltip-container">
                     <SiNetapp className="text-3xl text-blue-500"/>
+                    <span className="tooltip-text">NetApp</span>
                 </motion.div>
             </motion.div>
         </div>
